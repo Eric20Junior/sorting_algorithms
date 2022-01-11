67e40a8a0df091bcc8f670ac1cba57eb3f3d52e5
Eric20Junior <eric20junior@gmail.com>
2022-01-11 12:21:46 -0800

    new update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,78 +1,69 @@
-
 #include "sort.h"
-#include <stdio.h>
-
-size_t swap(int *array, size_t size, size_t idx1, size_t idx2);
-void partition(int *array, size_t size, size_t left, size_t right);
-
 /**
- * quick_sort - Sort an array of integers in ascending order
- * Description: Use Hoare's partition scheme quicksort
- * pivot should always be the last element of the partition being sorted.
- * You're expected to print the array after each time you swap 2 elements.
+ * partition - partitions the array
+ * @array: array to take in
+ * @start: start of array;
+ * @end: end of array
+ * @size: full size of array
+ * Return: position of pivot
+ */
+int partition(int *array, int start, int end, int size)
+{
+	int pivot = array[end];
+	int i = start, j, temp;
+
+	for (j = start; j < end; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (i != end)
+	{
+		temp = array[i];
+		array[i] = array[end];
+		array[end] = temp;
+		print_array(array, size);
+	}
+	printf("return i=%d\n", i);
+	return (i);
+}
+/**
+ * quickSort - quick sorts with recursion
+ * @array: array to sort through
+ * @start: start of array or subarray
+ * @end: end of array or subarray
+ * @size: size of full array
+ */
+void quickSort(int *array, int start, int end, int size)
+{
+	int pivot;
+
+	if (start < end)
+	{
+		pivot = partition(array, start, end, size);
+		printf("first recursive, start [%d] to pivot-1[%d]\n", start, pivot-1);
+		quickSort(array, start, pivot - 1, size);
+		printf("second recursive, pivot+1 [%d] to end [%d]\n", pivot+1, end);
+		quickSort(array, pivot + 1, end, size);
+	}
+}
+/**
+ * quick_sort - quick sorts an array
+ * @array: array to sort
+ * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t lw, rw;
-
-	lw = 0;
-	rw = size - 1;
-
-	partition(array, size, lw, rw);
-}
-
-void partition(int *array, size_t size, size_t left, size_t right)
-{
-	size_t i, j, pivot;
-
-	printf("?");
-
-	if (left >= right)
-	{
-		printf("Hi");
+	if (array == NULL || size < 2)
 		return;
-	}
-
-	pivot = right;
-	while (array[i] < array[pivot] && i < pivot)
-		i++;
-
-	if (i < right)
-	{
-		printf("A");
-		pivot = swap(array, size, i, pivot);
-
-		j = right - 1;
-		while (i < j)
-		{
-			if (array[j] < array[pivot])
-			{
-				swap(array, size, i, j);
-				i++;
-			}
-			j--;
-		}
-	}
-	else
-		pivot = i--;
-
-	printf("B");
-
-	swap(array, size, pivot, i - 1);
-
-	partition(array, size, left, i - 2);
-	partition(array, size, i, right);
-}
-
-size_t swap(int *array, size_t size, size_t idx1, size_t idx2)
-{
-	int tmp;
-
-	tmp = array[idx1];
-	array[idx1] = array[idx2];
-	array[idx2] = tmp;
-
-	print_array(array, size);
-
-	return idx1;
+	quickSort(array, 0, size - 1, size);
 }
